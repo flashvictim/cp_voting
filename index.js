a5c7fd5f9d51c0a6cdfc999fcd92fa0d9ec9fb92
flashvictim <andrej.hlavacka@gmail.com>
2014-08-25 14:32:03 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+var WebSocketServer = require("ws").Server;
+var http = require("http");
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 8002;
+
+app.use(express.static(__dirname + "/"));
+
+var server = http.createServer(app);
+server.listen(port);
+
+console.log("http server listening on %d", port);
+
+var wss = new WebSocketServer({server: server});
+console.log("websocket server created");
+
+wss.broadcast = function(data) {
+	for (var i in this.clients)
+    	this.clients[i].send(data);
+};
+
+wss.on('connection', function(ws) {
+	ws.on('message', function(message) {
+		wss.broadcast(message);
+	});
+});
